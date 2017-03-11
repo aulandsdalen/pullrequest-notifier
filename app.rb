a686903057b46109eff187cd9bf159160046471a
aulandsdalen <aulandsdalen@gmail.com>
2017-03-11 12:45:35 +0300

    new route names

--- app.rb
+++ app.rb
@@ -6,11 +6,17 @@ require 'sinatra'
 set :views, settings.root + '/views'
 DB = Sequel.connect('sqlite://pr.db')
 
-
 get '/' do
+	'visit /requests'
+end
+
+get '/requests' do
 	request_table = DB[:pull_request]
 	requests = request_table.all
 	haml :index, :locals => {:reqs => requests}
+end
+
+get '/requests/:creator' do 
 end
 
 post '/gh-event' do
