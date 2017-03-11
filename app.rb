da45dc3dc2ea09edf0661ef92231f9234d54e048
aulandsdalen <aulandsdalen@gmail.com>
2017-03-11 11:32:00 +0300

    / url handler added

--- app.rb
+++ app.rb
@@ -4,3 +4,13 @@ require 'json'
 require 'sinatra'
 
 DB = Sequel.connect('sqlite://pr.db')
+
+
+get '/' do
+	requests = DB[:pull_request]
+
+end
+
+post '/gh-event' do
+	logger.info "got request: #{params}"
+end
